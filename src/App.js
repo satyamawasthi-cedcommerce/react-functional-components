8aaecc0158fae1bd424711021c240fb68c8b934e
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-07-13 11:53:52 +0530

    task-1 (C) Make a functional component as child of App.js and pass your name as prop.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Hello from './components/Hello';
 import Helloarrow from './components/Helloarrow';
+import Myname from './components/Myname';
 import List1 from './components/List1';
 import List2 from './components/List2';
 
@@ -10,6 +11,8 @@ function App() {
       <Hello />
       <hr/>
       <Helloarrow/>
+      <hr/>
+      <Myname name = "Satyam" />
       <hr/>
       <List1/>
       <hr/>
---
+++ src/components/Myname.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Myname(Satyam) {
+  return (
+    <h1>Hello, {Satyam.name}</h1>
+  )
+}
