49d23b99d9f38dfdfd49ce9f1212ae3cb2aeff03
satyamawasthi-cedcommerce <satyamawasthi@cedcoss.com>
2022-07-13 11:38:44 +0530

    task-2 hello World Arrow funtion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hello from './components/Hello';
-
+import Helloarrow from './components/Helloarrow';
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <hr/>
+      <Helloarrow/>
       <hr/>
     </div>
   );
